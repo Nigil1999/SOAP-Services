9091381a9cd350f03aa187b97e8a2dfb85073418
Nigil1999 <nigilmr1999@gmail.com>
2022-01-12 10:34:08 +0530

    added soap service code

--- src/main/java/com/th/ws/demo/App.java
+++ src/main/java/com/th/ws/demo/App.java
@@ -1,6 +1,6 @@
 package com.th.ws.demo;
 
-import org.slf4j.Logger;
+/*import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 
@@ -15,5 +15,45 @@ public class App {
 		LOG.info("Start");
 		SpringApplication.run(App.class, args);
 		LOG.info("Running...");
+	}
+}*/
+
+/*package com.th.ws.consume.demo;*/
+
+import com.th.ws.demo.model.EmployeeClient;
+import https.www_torryharris_com.soap_ws_demo.GetEmployeeResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class App {
+
+	Logger LOG = LoggerFactory.getLogger(this.getClass());
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Bean
+	CommandLineRunner lookup(EmployeeClient quoteClient) {
+		return args -> {
+
+			GetEmployeeResponse response = quoteClient.getEmployee(3);
+			LOG.info(response.getEmployeeType().getEmployeeId() + " " + response.getEmployeeType().getFirstName() + " "
+					+ response.getEmployeeType().getSalary());
+
+//			GetAllEmployeesResponse response2 = quoteClient.getAllEmployees();
+//			response2.getEmployeesType()
+//					.forEach(obj -> LOG.info(obj.employeeId + " " + obj.firstName + " " + obj.salary));
+
+			GetEmployeeResponse response3 = quoteClient.addEmployee();
+			LOG.info(response3.getEmployeeType().getEmployeeId() + " " + response3.getEmployeeType().getFirstName()
+					+ " " + response3.getEmployeeType().getSalary());
+
+		};
 	}
 }
---
+++ src/main/java/com/th/ws/demo/model/EmployeeClient.java
@@ -0,0 +1,50 @@
+
+package com.th.ws.demo.model;
+
+import https.www_torryharris_com.soap_ws_demo.AddEmployeeRequest;
+import https.www_torryharris_com.soap_ws_demo.GetAllEmployeesResponse;
+import https.www_torryharris_com.soap_ws_demo.GetEmployeeRequest;
+import https.www_torryharris_com.soap_ws_demo.GetEmployeeResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
+
+public class EmployeeClient extends WebServiceGatewaySupport {
+
+	private static final Logger log = LoggerFactory.getLogger(EmployeeClient.class);
+
+	public GetEmployeeResponse getEmployee(int employeeId) {
+		GetEmployeeRequest request = new GetEmployeeRequest();
+		request.setEmployeeId(employeeId);
+		log.info("Requesting ... " + employeeId);
+		GetEmployeeResponse response = (GetEmployeeResponse) getWebServiceTemplate().marshalSendAndReceive(
+				"http://localhost:8099/ws/employees", request,
+				new SoapActionCallback("https://www.torryharris.com/soap-ws-demo/GetEmployeeRequest"));
+		return response;
+	}
+
+	public GetAllEmployeesResponse getAllEmployees() {
+		GetEmployeeRequest request = new GetEmployeeRequest();
+		log.info("Requesting ... ");
+		GetAllEmployeesResponse response = (GetAllEmployeesResponse) getWebServiceTemplate().marshalSendAndReceive(
+				"http://localhost:8099/ws/employees", request,
+				new SoapActionCallback("https://www.torryharris.com/soap-ws-demo/GetEmployeeRequest"));
+		return response;
+	}
+	
+	public GetEmployeeResponse addEmployee() {
+		AddEmployeeRequest request = new AddEmployeeRequest();
+		request.setEmployeeId(10);
+		request.setFirstName("Donu");
+		request.setSalary(65000);
+		
+		log.info("Requesting ... ");
+		GetEmployeeResponse response = (GetEmployeeResponse) getWebServiceTemplate().marshalSendAndReceive(
+				"http://localhost:8099/ws/employees", request,
+				new SoapActionCallback("https://www.torryharris.com/soap-ws-demo/GetEmployeeRequest"));
+		return response;
+	}
+
+
+}
